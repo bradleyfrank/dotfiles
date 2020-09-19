930ad17fbc46e89dde3e5d54da11b39988781042
Bradley Frank <bradfrank@fastmail.com>
2020-09-19 17:02:46 -0400

    Refactored for flexibility and readability.

--- run.sh
+++ run.sh
@@ -3,16 +3,24 @@
 # Set global variables
 SUDOPW=""
 SKIP_TAGS="work_only"
+SYSTEM_TYPE="$(uname -s | tr '[:upper:]' '[:lower:]')"
+ANSIBLE_REPO="https://github.com/bradleyfrank/dotfiles.git"
 
-# Restores echo command on Cntl-c
+
+# Restores echo command in case of Cntl-c
 trap "stty echo" INT
 
-# Remove global variables on exit
+# Perform cleanup on exit
 trap cleanup EXIT
+
 
 cleanup() {
   command rm -rf "$tmp_checkout"
   command rm -rf "$tmp_json"
+  unset "$SUDOPW"
+  unset "$SKIP_TAGS"
+  unset "$SYSTEM_TYPE"
+  unset "$ANSIBLE_REPO"
 }
 
 enter_sudo_pw() {
@@ -28,63 +36,69 @@ not_supported() {
   exit 1
 }
 
+bootstrap_os() {
+  case "$SYSTEM_TYPE" in
+    darwin) bootstrap_macos ;;
+     linux) bootstrap_linux ;;
+         *) not_supported   ;;
+  esac
+}
+
 bootstrap_macos() {
   homebrew_url="https://raw.githubusercontent.com/Homebrew/install/master/install"
+  softwareupdate --install --all
   xcode-select --print-path >/dev/null 2>&1 || xcode-select --install
   [ ! -x /usr/local/bin/brew ] && /usr/bin/ruby -e "$(curl -fsSL "$homebrew_url")"
   [ ! -x /usr/local/bin/ansible ] && brew install ansible git
 }
 
 bootstrap_linux() {
-  case "$(sed -rn 's/^ID=([a-z]+)/\1/p' /etc/os-release)" in
-    fedora) printf "%s" "$SUDOPW" | sudo -S dnf install -y ansible git ;;
-         *) not_supported ;;
-  esac
+  tmp_script="$(mktemp)"
+
+  if type dnf > /dev/null; then
+    printf "dnf clean all\ndnf upgrade -y\ndnf install -y ansible git" > "$tmp_script"
+  else
+    not_supported
+  fi
+
+  chmod 0755 "$tmp_script"
+  printf "%s" "$SUDOPW" | sudo -S sh "$(tmp_script)"
 }
 
-main() {
-  ansible_repo="https://github.com/bradleyfrank/dotfiles.git"
-  system_os="$(uname -s | tr '[:upper:]' '[:lower:]')"
+run_ansible() {
   tmp_checkout="$(mktemp -d)"
   tmp_json="$(mktemp)"
 
-  # Prompt user for sudo password
-  enter_sudo_pw
-
-  # Bootstrap Ansible itself
-  case "$system_os" in
-    darwin) bootstrap_macos ;;
-     linux) bootstrap_linux ;;
-         *) not_supported   ;;
-  esac
-
-  # Make a json with sudo password to avoid escape quote hell
+  # Make a file with sudo password to avoid escape quote hell
   printf '{"ansible_become_pass":"%s"}' "$SUDOPW" > "$tmp_json"
 
   if ansible-pull \
-    --url "$ansible_repo" \
+    --url "$ANSIBLE_REPO" \
     --directory "$tmp_checkout" \
     --extra-vars "@$tmp_json" \
     --skip-tags "$SKIP_TAGS" \
-    playbooks/"$system_os".yml
+    playbooks/"$SYSTEM_TYPE".yml
   then
     [ -e "$HOME"/.dotfiles ] && rm -rf "$HOME"/.dotfiles
     mv "$tmp_checkout" "$HOME"/.dotfiles
-    exit 0
+    return 0
   else
     printf "%s\n" "Ansible run failed" >&2
-    exit 1
+    return 1
   fi
 }
 
-# Parse arguments for home/work deployment
+
 while getopts ':wh' flag; do
-  case "${flag}" in
+  case "$flag" in
     w) SKIP_TAGS="home_only" ;;
     h) SKIP_TAGS="work_only" ;;
     *) printf "%s\n" "Requires [-w|-h], aborting..." >&2 ; exit 1 ;;
   esac
 done
 
-# Workhorse function
-main
+enter_sudo_pw
+bootstrap_os
+run_ansible
+
+exit $?
