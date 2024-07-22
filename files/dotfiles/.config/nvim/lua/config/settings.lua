9f7bc62e712dc9ef374680c801bc1ba7fd953b4a
Brad Frank <bfrank@oreilly.com>
2024-07-22 15:03:04 -0400

    feat(nvim): use system clipboard

--- files/dotfiles/.config/nvim/lua/config/settings.lua
+++ files/dotfiles/.config/nvim/lua/config/settings.lua
@@ -14,15 +14,18 @@ vim.opt.backup = false
 vim.undodir = os.getenv("HOME") .. "/.vim.undodir"
 vim.opt.undofile = true
 
--- set incremental search. This helps immensly with tricky searches
+-- set incremental search
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 -- fast update time
 vim.opt.updatetime = 50
 
--- color column to 80 characters
+-- color column to 100 characters
 vim.opt.colorcolumn = "100"
 
 -- filetype trigger
 vim.opt.filetype='on'
+
+-- copy to system clipboard
+vim.api.nvim_set_option("clipboard", "unnamed")
